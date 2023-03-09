e409b52b1fade6adf2d7f9e2466abd42d4c1ad14
JIOjosBG <jjjooosssiiifff@gmail.com>
2023-03-09 16:01:12 +0200

    feat: remove user, add date to measurement

--- hardhat/contracts/Storage.sol
+++ hardhat/contracts/Storage.sol
@@ -13,22 +13,19 @@ contract Storage is Ownable{
         require(medics[msg.sender],"Not medic");
         _;
     }
-    struct User{
-        address addr;
-        uint measurementCount;
-    }
 
     struct Measurement{
         bytes UId;
+        uint date;
         bytes data;
         uint MId;
     }
+
     mapping(address=>bool) medics;
-    mapping(address=>User) users;
     Measurement[] measurements;
 
     function createMeasurement(bytes calldata _data, bytes calldata _UId, uint _MId) public onlyMedic {
-        Measurement memory m = Measurement(_UId, _data, _MId);
+        Measurement memory m = Measurement(_UId, block.timestamp, _data, _MId);
         measurements.push(m);
         emit createdMeasurement(_MId, _UId);
     }
@@ -37,6 +34,5 @@ contract Storage is Ownable{
         medics[_medic]=true;
         emit addedMedic(_medic);
     }
-
 
 }
