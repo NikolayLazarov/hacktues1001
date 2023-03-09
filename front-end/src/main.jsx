00a8e6bb467896b37a6c13cc9196d8b7d707ab0c
Nikolay Lazarov <nikilazt7@gmail.com>
2023-03-09 22:52:02 +0200

    merged two front-ends

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import Data from './layout/Data'
 import './index.css'
+import EntryForm from './components/EntryForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <EntryForm />
     <Data />
+
   </React.StrictMode>,
 )
