e09a2b497ab83c35f53ac7925e59bd148e33e66d
JIOjosBG <jjjooosssiiifff@gmail.com>
2023-03-09 16:15:41 +0200

    feat: added deployed smart contract info

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY]
+    }
+  }
 };
